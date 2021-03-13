2836d34324f7cc548453ab074a5f3673fd4ae4ac
rudhshah <59837096+rudhshah@users.noreply.github.com>
2021-03-13 14:17:36 -0500

    Update Two-sum.cpp

--- Two-sum.cpp
+++ Two-sum.cpp
@@ -1,1 +1,18 @@
-
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        map<int,int> match;
+        vector<int> result;
+        int temp =0;
+        for(int i =0; i < nums.size(); i++){
+            if(match.find(nums[i]) != match.end()){
+                result.push_back(match[nums[i]]);
+                result.push_back(i);
+            }
+               else{
+                   match[target - nums[i]] = i; 
+               }
+        }
+        return result;
+    }
+};
